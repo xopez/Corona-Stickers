0787e285613683145b3f691877fcc37f664cd718
xopez <saschamoser1994@hotmail.de>
2020-03-21 16:26:52 +0100

    Initial commit

---
+++ app/src/main/java/de/saschamoser/coronaicons/StickerPreviewViewHolder.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) WhatsApp Inc. and its affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package de.saschamoser.coronaicons;
+
+import android.view.View;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import de.saschamoser.coronaicons.R;
+import com.facebook.drawee.view.SimpleDraweeView;
+
+class StickerPreviewViewHolder extends RecyclerView.ViewHolder {
+
+    final SimpleDraweeView stickerPreviewView;
+
+    StickerPreviewViewHolder(final View itemView) {
+        super(itemView);
+        stickerPreviewView = itemView.findViewById(R.id.sticker_preview);
+    }
+}
